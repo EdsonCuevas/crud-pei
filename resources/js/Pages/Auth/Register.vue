cd2ef7df0a40501469717270761b549f0c1015a3
EdsonCuevas <efelix@ucol.mx>
2024-08-29 13:31:37 -0600

    add combobox role in register

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -23,50 +23,68 @@ const submit = () => {
 
 <template>
 	<GuestLayout>
+
 		<Head title="Register" />
-		
+
 		<div class="flex flex-col overflow-y-auto md:flex-row">
 			<div class="h-32 md:h-auto md:w-1/2">
-				<img aria-hidden="true" class="object-cover w-full h-full" src="/images/create-account-office.jpeg" alt="Office" />
+				<img aria-hidden="true" class="object-cover w-full h-full" src="/images/create-account-office.jpeg"
+					alt="Office" />
 			</div>
-			
+
 			<div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
 				<div class="w-full">
 					<h1 class="mb-4 text-xl font-semibold text-gray-700">
 						Create account
 					</h1>
-					
+
 					<form @submit.prevent="submit">
 						<div>
 							<InputLabel for="name" value="Name" />
-							<TextInput id="name" type="text" class="block w-full mt-1" v-model="form.name" required autofocus autocomplete="name" />
+							<TextInput id="name" type="text" class="block w-full mt-1" v-model="form.name" required
+								autofocus autocomplete="name" />
 							<InputError class="mt-2" :message="form.errors.name" />
 						</div>
-						
+
 						<div class="mt-4">
 							<InputLabel for="email" value="Email" />
-							<TextInput id="email" type="email" class="block w-full mt-1" v-model="form.email" required autocomplete="username" />
+							<TextInput id="email" type="email" class="block w-full mt-1" v-model="form.email" required
+								autocomplete="username" />
 							<InputError class="mt-2" :message="form.errors.email" />
 						</div>
-						
+
 						<div class="mt-4">
 							<InputLabel for="password" value="Password" />
-							<TextInput id="password" type="password" class="block w-full mt-1" v-model="form.password" required autocomplete="new-password" />
+							<TextInput id="password" type="password" class="block w-full mt-1" v-model="form.password"
+								required autocomplete="new-password" />
 							<InputError class="mt-2" :message="form.errors.password" />
 						</div>
-						
+
 						<div class="mt-4">
 							<InputLabel for="password_confirmation" value="Confirm Password" />
-							<TextInput id="password_confirmation" type="password" class="block w-full mt-1" v-model="form.password_confirmation" required autocomplete="new-password" />
+							<TextInput id="password_confirmation" type="password" class="block w-full mt-1"
+								v-model="form.password_confirmation" required autocomplete="new-password" />
 							<InputError class="mt-2" :message="form.errors.password_confirmation" />
 						</div>
-						
+
+						<div class="mt-4">
+							<InputLabel for="role" value="Role" />
+							<select id="role" class="block w-full mt-1" v-model="form.role" required>
+								<option value="" disabled>Select a role</option>
+								<option value="volunt">Voluntario</option>
+								<option value="donador">Donador</option>
+								<option value="benefi">Beneficiario</option>
+							</select>
+							<InputError class="mt-2" :message="form.errors.role" />
+						</div>
+
 						<div class="flex items-center justify-end mt-4">
 							<Link :href="route('login')" class="text-sm text-gray-600 underline  hover:text-gray-900">
-								Already registered?
+							Already registered?
 							</Link>
-							
-							<PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+
+							<PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }"
+								:disabled="form.processing">
 								Register
 							</PrimaryButton>
 						</div>
