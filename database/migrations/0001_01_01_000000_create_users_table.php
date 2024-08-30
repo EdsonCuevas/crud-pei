b95074c7c17eb79bdcf1ef241edfa82a3321a4de
EdsonCuevas <efelix@ucol.mx>
2024-08-30 17:40:51 -0600

    primera prueba migracion

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role', 20);
+            $table->string('photo');
+            $table->string('numphone', 20);
             $table->rememberToken();
             $table->timestamps();
         });
