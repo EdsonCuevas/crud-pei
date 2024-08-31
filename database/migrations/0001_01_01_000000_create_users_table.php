5159655acdf28ee2d05a6e17970c67cc33aba9e8
EdsonCuevas <efelix@ucol.mx>
2024-08-30 18:30:23 -0600

    ultimas migraciones

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role', 20);
-            $table->string('photo');
-            $table->string('numphone', 20);
+            $table->string('role', 20)->nullable();
+            $table->string('photo')->nullable();
+            $table->string('numphone', 20)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
