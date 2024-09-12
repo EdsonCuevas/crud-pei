8d63e9f200b36f5df2ac1dc07db817a83ef1d80c
Gustavo <gceja2@ucol.mx>
2024-09-12 10:36:33 -0600

    blue botton

--- resources/js/Pages/PanelCoordi/Index.vue
+++ resources/js/Pages/PanelCoordi/Index.vue
@@ -128,10 +128,7 @@ const ok = (m) => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
-            </div>
-            
-           
-            
+            </div> 
         </div>
 		
 		<div class="w-full overflow-hidden rounded-lg border shadow-md ">
@@ -165,17 +162,25 @@ const ok = (m) => {
                                 <td class="px-4 py-3 text-sm">
                                 </td>
                                 <td class="px-4 py-3 text-sm">
-                                    <WarningButton @click="openModalForm(1)">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                        </svg>
-                                    </WarningButton>
+                                    
+                            <WarningButton @click="openModalForm(1)">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                </svg>
+                            </WarningButton>
+
+                            <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600  ml-2" @click="openModalForm(1)">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                                </svg>
+                            </button>
                                 </td>
 							</tr>
                     </tbody>
                 </table>
             </div>
 		</div>
+
         <Modal :show="showModalView" @close="closeModalView">
             <div class="p-6">
                 Programas a los que esta inscrito:
