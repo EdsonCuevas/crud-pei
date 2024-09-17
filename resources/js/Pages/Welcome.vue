021c1cc73d86b2447c54a7833681ad07f9038c67
Edson <efelix.cuevas@gmail.com>
2024-09-17 00:00:58 -0600

    fix welcome

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -55,7 +55,7 @@ function handleImageError() {
                     <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
                         <Link
                             v-if="$page.props.auth.user"
-                            :href="route('dashboard')"
+                            :href="route('')"
                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
                             Dashboard
