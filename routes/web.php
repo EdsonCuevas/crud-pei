5aa53a516997b31a37e4e6f7d9900e9a5cf5624f
Edson <efelix.cuevas@gmail.com>
2024-09-21 01:12:01 -0600

    fix welcome pages

--- routes/web.php
+++ routes/web.php
@@ -42,12 +42,15 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
+    /*
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
+    */
+    return redirect('/login');
 });
 
 
