26df7502a0a0fb36ba93b813d39eab14f64e146e
Edson <efelix@ucol.mx>
2024-09-30 09:31:48 -0600

    fix margin in button read

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -5,7 +5,7 @@
     <slot name="icon" />
     <Link :href="href"
       class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800">
-    <span class="ml-4">
+    <span>
       <slot />
     </span>
     </Link>
