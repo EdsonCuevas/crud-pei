1c0048c48a21c63914e552891d7b3c437737db3b
Edson <efelix.cuevas@gmail.com>
2024-10-07 01:09:14 -0600

    fix recursos benef panel

--- app/Http/Controllers/BenefRecursosController.php
+++ app/Http/Controllers/BenefRecursosController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Program;
+use App\Models\User;
 use Inertia\Inertia;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
 class BenefRecursosController extends Controller
@@ -20,44 +18,10 @@ class BenefRecursosController extends Controller
             return redirect()->route('401')->with('error', 'No tienes acceso a esta página.');
         }
 
-        // Filtra los usuarios que tengan el rol 'benefi' y carga sus programas
-        $programs = Program::all();
+        $programs = $user->programs()->with(['creator', 'coordinator'])->get();
 
         return Inertia::render('Benef/Recursos', [
             'programas' => $programs
         ]);
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|max:60',
-            'email' => 'required|max:60',
-            'password' => 'required|min:8',
-            'phone' => 'required|max:20',
-        ]);
-        $beneficiarios = new User($request->except('password'));
-        $beneficiarios->role = 'benefi';
-        $beneficiarios->password = Hash::make($request->password);
-        $beneficiarios->save();
-        return redirect('beneficiaries');
-    }
-
-    public function update(Request $request, User $beneficiarios)
-    {
-        $request->validate([
-            'name' => 'required|max:60',
-            'email' => 'required|max:60',
-            'phone' => 'required|max:20',
-        ]);
-
-        $beneficiarios->update($request->input());
-        return redirect('beneficiaries');
-    }
-
-    public function destroy(User $beneficiarios)
-    {
-        $beneficiarios->delete();
-        return redirect('beneficiaries');
     }
 }
--- resources/js/Pages/Benef/Recursos.vue
+++ resources/js/Pages/Benef/Recursos.vue
@@ -11,59 +11,33 @@ const props = defineProps({
 		type: Array
 	},
 });
-const form = useForm({
-     name:'',
-     email:'',
-     phone:'',
-     password:'',
-});
 
-const v = ref({
-    id:'',
-    name:'',
-    phone:'',
-    password:'',
-    programs:[]
+
+// Variables para el título y la descripción del programa
+const selectedProgram = ref({
+    title: '',
+    description: '',
+    image: ''
 });
 
 const showModalView = ref(false);
-const showModalForm = ref(false);
-const showModalDel = ref(false);
-const operation = ref(1);
-const msj = ref('');
-const classMsj = ref('hidden');
 
-const openModalView = (a) => {
-    v.value.name = a.name;
-    v.value.phone = a.phone;
-    v.value.programs = a.programs;
+const openModalView = (programa) => {
+    
+    // Pasamos el título y la descripción del programa
+    selectedProgram.value.title = programa.title;
+    selectedProgram.value.description = programa.description;
+    selectedProgram.value.image = programa.image;
+
     showModalView.value = true;
 }
-
 
 const closeModalView = () => {
     showModalView.value = false;
 }
-const closeModalForm = () => {
-    showModalForm.value = false;
-    form.reset();
-}
-const closeModalDel = () => {
-    showModalDel.value = false;
-}
-
-
-const ok = (m) => {
-    if(operation.value == 2){
-        closeModalForm();
-    }
-    closeModalDel();
-    form.reset();
-    msj.value = m;
-    classMsj.value = 'block';
-}
 
 </script>
+
 
 <template>
 	<Head title="Beneficiario" />
@@ -73,13 +47,6 @@ const ok = (m) => {
 			Recursos
             <br>
             <br>
-            <!--
-            <DarkButton @click="openModalForm(1)">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
-                </svg>
-            </DarkButton>
-            -->
 		</template>
 		
 		<div class="w-full overflow-hidden rounded-lg border shadow-md ">
@@ -87,19 +54,18 @@ const ok = (m) => {
                 <table class="w-full whitespace-no-wrap">
                     <thead>
                         <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b bg-gray-50">
-                            <th class="px-4 py-3">Beca</th>
-                            <th class="px-4 py-3">Administrado<br>por</th>
-
+                            <th class="px-4 py-3">Programa</th>
+                            <th class="px-4 py-3">Administrado por</th>
                             <th class="px-4 py-3">Detalles</th>
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y">
                         <tr v-for="programa in programas" :key="programa.id" class="text-gray-700">  
 								<td class="px-4 py-3 text-sm">
-									{{ programa.name }}
+									{{ programa.title }}
 								</td>
 								<td class="px-4 py-3 text-sm">
-									
+									{{ programa.coordinator.name }}
 								</td>
                                 <td class="px-4 py-3 text-sm">
                                     <SecondaryButton @click="openModalView(programa)">
@@ -114,17 +80,16 @@ const ok = (m) => {
                 </table>
             </div>
 		</div>
+
+        <!-- Modal de vista del programa seleccionado -->
         <Modal :show="showModalView" @close="closeModalView">
             <div class="p-6">
-                Informacion de la Beca:
-                <ol v-for="programa in programas" :key="programa.id">
-                    <li class="text-lg font-medium text-gray-900">
-                       Beca: {{ programa.name }}
-                    </li><br>
-                    <li class="text-lg text-gray-900">
-                       Descripcion: {{ programa.description }}
-                    </li>
-                </ol>
+                <h2 class="text-xl font-bold">{{ selectedProgram.title }}</h2>
+                <p class="mt-2">{{ selectedProgram.description }}</p>
+                <img v-if="selectedProgram.image" 
+                 :src="`/storage/img/${selectedProgram.image}`" 
+                 alt="Imagen del programa" 
+                 class="mt-4 w-full h-[500px] rounded-lg">
             </div>
             <div class="m-6 flex justify-end">
                 <SecondaryButton @click="closeModalView">Cerrar</SecondaryButton>
