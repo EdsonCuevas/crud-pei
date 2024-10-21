ef580ddd26ba5c9a564a39c707daf92708be141f
Edson <efelix@ucol.mx>
2024-10-21 07:42:03 -0600

    add birthdate and rfc crud admin

--- resources/js/Pages/Admin/Users/Coordinators.vue
+++ resources/js/Pages/Admin/Users/Coordinators.vue
@@ -25,6 +25,8 @@ const form = useForm({
     name: '',
     email: '',
     phone: '',
+    date: '',
+    rfc: '',
     password: '',
     role_id: '',
 });
@@ -33,6 +35,8 @@ const v = ref({
     id: '',
     name: '',
     phone: '',
+    rfc: '',
+    date: '',
     password: '',
     programs: []
 });
@@ -65,6 +69,8 @@ const openModalForm = (op, a) => {
         form.password = '';
         form.role_id = a.role_id;
         form.phone = a.phone;
+        form.date = a.birthdate;
+        form.rfc = a.rfc;
         v.value.id = a.id;
     }
 }
@@ -131,7 +137,7 @@ const exportUsers = () => {
 
 <template>
 
-    <Head title="Coordinadores" />
+    <Head title="Coordinators" />
 
     <AuthenticatedLayout>
         <template #header>
@@ -306,6 +312,20 @@ const exportUsers = () => {
                         </svg>
                     </InputGroup>
                     <InputError class="mt-1" :message="form.errors.phone"></InputError>
+
+                    <InputGroup :text="'Fecha de Usuario'" v-model="form.date" :type="'date'">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 8.25v-1.5m0 1.5c-1.355 0-2.697.056-4.024.166C6.845 8.51 6 9.473 6 10.608v2.513m6-4.871c1.355 0 2.697.056 4.024.166C17.155 8.51 18 9.473 18 10.608v2.513M15 8.25v-1.5m-6 1.5v-1.5m12 9.75-1.5.75a3.354 3.354 0 0 1-3 0 3.354 3.354 0 0 0-3 0 3.354 3.354 0 0 1-3 0 3.354 3.354 0 0 0-3 0 3.354 3.354 0 0 1-3 0L3 16.5m15-3.379a48.474 48.474 0 0 0-6-.371c-2.032 0-4.034.126-6 .371m12 0c.39.049.777.102 1.163.16 1.07.16 1.837 1.094 1.837 2.175v5.169c0 .621-.504 1.125-1.125 1.125H4.125A1.125 1.125 0 0 1 3 20.625v-5.17c0-1.08.768-2.014 1.837-2.174A47.78 47.78 0 0 1 6 13.12M12.265 3.11a.375.375 0 1 1-.53 0L12 2.845l.265.265Zm-3 0a.375.375 0 1 1-.53 0L9 2.845l.265.265Zm6 0a.375.375 0 1 1-.53 0L15 2.845l.265.265Z" />
+                        </svg>
+                    </InputGroup>
+                    <InputError class="mt-1" :message="form.errors.date"></InputError>
+
+                    <InputGroup :text="'RFC'" v-model="form.rfc">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v17.25m0 0c-1.472 0-2.882.265-4.185.75M12 20.25c1.472 0 2.882.265 4.185.75M18.75 4.97A48.416 48.416 0 0 0 12 4.5c-2.291 0-4.545.16-6.75.47m13.5 0c1.01.143 2.01.317 3 .52m-3-.52 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.988 5.988 0 0 1-2.031.352 5.988 5.988 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L18.75 4.971Zm-16.5.52c.99-.203 1.99-.377 3-.52m0 0 2.62 10.726c.122.499-.106 1.028-.589 1.202a5.989 5.989 0 0 1-2.031.352 5.989 5.989 0 0 1-2.031-.352c-.483-.174-.711-.703-.59-1.202L5.25 4.971Z" />
+                        </svg>
+                    </InputGroup>
+                    <InputError class="mt-1" :message="form.errors.rfc"></InputError>
 
                 </div>
             </div>
