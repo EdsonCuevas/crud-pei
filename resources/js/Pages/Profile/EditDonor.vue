504b6e42db855a44bf1a9060386c034df886c7bb
ricardogalvan19 <rcabanillas@ucol.mx>
2024-10-31 11:16:09 -0600

    space-profile

--- resources/js/Pages/Profile/EditDonor.vue
+++ resources/js/Pages/Profile/EditDonor.vue
@@ -36,6 +36,7 @@ defineProps({
 			<div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
 				<DeleteUserForm class="max-w-xl" />
 			</div>
+			<br>
 		</div>
 	</AuthenticatedLayout>
 </template>
